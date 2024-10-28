625f6c70e6e9a7deda048507c349d0dead8c9cfe
duaa-art <duaaalrefai@gmail.com>
2024-10-28 12:02:18 +0100

    Update Car.cs

--- Prague Parking 2.0 – OOP-version/Car.cs
+++ Prague Parking 2.0 – OOP-version/Car.cs
@@ -8,9 +8,10 @@ namespace Prague_Parking_2._0___OOP_version
 {
     internal class Car : Vehicle
     {
-        // genskap som returnerar typen av fordon,
-        // i det här fallet "Car".
-        public override string Type => "Car";
+        // genskap som returnerar typen av fordon
+        public override string Type
+        {
+        get { return "Car";}
         // anropar basklassens konstruktor och
         // skickar registreringsnumret till basklassen Vehicle
         public Car(string regNumber) : base(regNumber) { }

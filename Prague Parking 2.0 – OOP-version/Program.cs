d17f004e661c8bb9ebe2d6f59135fd208c023743
duaa-art <duaaalrefai@gmail.com>
2024-10-29 10:10:25 +0100

    Update Program.cs

--- Prague Parking 2.0 – OOP-version/Program.cs
+++ Prague Parking 2.0 – OOP-version/Program.cs
@@ -1,1 +1,17 @@
+public class ParkingSpot
+{ public Vehicle Vehicle { get; set; }
+    public ParkingSpot(Vehicle vehicle) 
+    { Vehicle = vehicle; }
+}
+
+public class ParkingGarage
+{
+    private List<ParkingSpot> parkingSpots;
+    public ParkingGarage(int numSpots)
+    {
+        parkingSpots = new List<ParkingSpot>(numSpots);
+        for (int i = 0; i < numSpots; i++)
+        { parkingSpots.Add(null); }
+    }
+}
 ﻿
